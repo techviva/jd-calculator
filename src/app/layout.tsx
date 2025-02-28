392464c351f3aa8d3b2b43c3a376384438d36450
John-Dag Addy <jaedagy@gmail.com>
2025-02-28 14:32:45 +0000

    feat: install chakra-ui

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Provider } from "@/components/ui/provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Viva Job Calculator",
+  description: "Calculate your job's worth",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Provider>
+          {children}
+        </Provider>
+      </body>
+    </html>
+  );
+}
